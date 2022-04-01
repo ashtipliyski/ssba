677686f4dbd943ff49e8ac953a0928534a7cf447
Antoni Shtipliyski <a.shtipliyski@nullone.com>
2022-04-01 23:45:24 +0100

    Add VM skeleton

--- 04_building_a_vm/vm.py
+++ 04_building_a_vm/vm.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 
 def main():
-    pass
+    vm = {
+        "pc": 0,      # programme counter - refers to address of current instruction
+        "reg1": 0x00, # register 1 - holds value for manipulation by VM
+        "reg2": 0x00  # register 2 - holds value for manipulation by VM
+    };
+
+    memory = [
+        0x01, 0x01, 0x10,
+        0x01, 0x02, 0x12,
+        0x03, 0x01, 0x02,
+        0x02, 0x01, 0x0e,
+        0xff,
+        0x00,
+        0x00, 0x00,
+        0xa1, 0x14,
+        0x0c, 0x00        
+    ]
+
+    while memory[vm['pc']] != 0xFF:
+        # check current instruction
+
+        # update PC, depending on current instruction
+        pass
 
 if __name__ == "__main__":
     main()

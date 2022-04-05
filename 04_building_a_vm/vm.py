b584dd0f7da70a3547035fb1c6f42caf6d761f56
Antoni Shtipliyski <a.shtipliyski@nullone.com>
2022-04-05 01:24:19 +0100

    Improve naming of register variables

--- 04_building_a_vm/vm.py
+++ 04_building_a_vm/vm.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
-def load_word(vm, mem, reg, addr):
+def load_word(regs, mem, reg, addr):
     # load value in addr to reg
-    vm[reg] = mem[addr] + 256 * mem[addr+1]
+    regs[reg] = mem[addr] + 256 * mem[addr+1]
 
-def store_word(vm, mem, reg, addr):
-    mem[addr] = vm[reg] % 256
-    mem[addr + 1] = vm[reg] // 256
+def store_word(regs, mem, reg, addr):
+    mem[addr] = regs[reg] % 256
+    mem[addr + 1] = regs[reg] // 256
 
-def add(vm, reg1, reg2):
-    vm[reg1] = vm[reg1] + vm[reg2]
+def add(regs, reg1, reg2):
+    regs[reg1] = regs[reg1] + regs[reg2]
 
-def sub(vm, reg1, reg2):
-    vm[reg1] = vm[reg1] - vm[reg2]
+def sub(regs, reg1, reg2):
+    regs[reg1] = regs[reg1] - regs[reg2]
 
 def print_mem(mem):
 
@@ -25,7 +25,7 @@ def print_mem(mem):
     print(f"|{'data and instructions' : ^42}|{'OUT' :^5}|{'IN1' :^5}|{'IN2' :^5} |")
 
 def main():
-    vm = [
+    registers = [
         0, # programme counter - refers to address of current instruction
         0, # general purpose register at 0x01
         0  # general register at 0x02
@@ -43,24 +43,24 @@ def main():
         0x0c, 0x00        
     ]
 
-    ins = memory[vm[0x00]]
+    ins = memory[registers[0x00]]
     while ins != 0xFF:
-        ins = memory[vm[0x00]]     # extract the current instruction from memory
-        op1 = memory[vm[0x00] + 1] # extract 1st operand for current instruction
-        op2 = memory[vm[0x00] + 2] # extract 2nd operand for current instruction
+        ins = memory[registers[0x00]]     # extract the current instruction from memory
+        op1 = memory[registers[0x00] + 1] # extract 1st operand for current instruction
+        op2 = memory[registers[0x00] + 2] # extract 2nd operand for current instruction
         
         # check current instruction
         if ins == 0x01:   # 0x01 = load_word
-            load_word(vm, memory, op1, op2)
+            load_word(registers, memory, op1, op2)
         elif ins == 0x02: # 0x02 = store_word
-            store_word(vm, memory, op1, op2)
+            store_word(registers, memory, op1, op2)
         elif ins == 0x03: # 0x03 = add
-            add(vm, op1, op2)
+            add(registers, op1, op2)
         elif ins == 0x04: # 0x04 = subtract
-            sub(vm, op1, op2)
+            sub(registers, op1, op2)
             
         # update PC to next instruction
-        vm[0x00] += 3
+        registers[0x00] += 3
 
     print_mem(memory)
 

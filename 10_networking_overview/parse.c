c6379fa8158b4e161c0092d48b0935ff64876a6b
Antoni Shtipliyski <a.shtipliyski@nullone.com>
2022-05-01 02:15:15 +0100

    Start constructing status printout for each packet and preparation for handling packet data and multiple packets

--- 10_networking_overview/parse.c
+++ 10_networking_overview/parse.c
@@ -66,28 +66,54 @@ int main ()
   printf("net: %lu\n", get_long(&stuff[20])); // 1 for ethernet
 
   // start a cycle of reading header + packet until EOF
-  while (1) {
-    short HEADSIZE = 128;
+  // while (1) {
+  for (int j = 0; j < 10; j++) {
+    short HEADSIZE = 16;
     unsigned char headbytes[HEADSIZE + 1]; // array needs and extra char at end for NUL
-    fgets(headbytes, HEADSIZE+1, f); // fgets reads up to N - 1 chars to make space for NUL at end
+    // fgets(headbytes, HEADSIZE+1, f); // fgets reads up to N - 1 chars to make space for NUL at end
 
+    // fgets seems to result in strange contents of headbytes so the individual bytes are
+    // read individually
+    for (int i = 0; i < HEADSIZE; i++) {
+      headbytes[i] = getc(f);
+      if (headbytes[i] == EOF) break;
+    }
+    headbytes[HEADSIZE+1] = 0; // manually set NUL at end
+
+    /*
     printf("0x%X\n", headbytes[HEADSIZE]);
     for (int i = 0; i < HEADSIZE; i++) {
       printf("head c = %02x\n", headbytes[i]);
-
-      if (i > 10) break;
+      // printf("head c = %02x\n", getc(f));
+      // if ((i+i) % 4 == 0 && i != 0) printf("--\n");
     }
-    
+    */
 
     time_t tim = get_long(headbytes);
+    struct tm * loc_tm = localtime(&tim);
     // time_t now = time(&now);
     // struct tm * loc_tm = localtime(&now);
     // printf("%lu\n", time);
-    printf("ts time: %s\n", asctime(localtime(&tim)));
+
+    printf("ts time: %s", ctime((&tim)));
     printf("ts_sec: %X\n", get_long(headbytes));
     printf("ts_usec: %lu\n", get_long(headbytes + 4));
     printf("incl_len: %lu\n", get_long(headbytes + 8));
     printf("orig_len: %lu\n", get_long(headbytes + 12));
-    break;
+    int PAYLOADSIZE = get_long(headbytes + 8) - HEADSIZE;
+
+    // generate summary of packet
+    printf("> %4d-%02d-%02d %02d:%02d:%02d ", loc_tm->tm_year + 1900,
+	   loc_tm->tm_mon + 1,
+	   loc_tm->tm_mday,
+	   loc_tm->tm_hour,
+	   loc_tm->tm_min,
+	   loc_tm->tm_sec);
+    printf("%d of bytes payload (%d read)\n", get_long(headbytes + 8), get_long(headbytes + 12));
+
+    // ---- parse packet payload ----
+    unsigned char data[PAYLOADSIZE + 1];
+    fgets(data, PAYLOADSIZE+1, f);
+    // break;
   }
 }

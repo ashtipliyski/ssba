391e2d15e2dd17dc07807ff8849ba08fb23d2077
Antoni Shtipliyski <a.shtipliyski@nullone.com>
2022-05-03 22:32:35 +0100

    Annotate encode/decode methods and refactor naming of structs

--- 11_dns/dns.c
+++ 11_dns/dns.c
@@ -17,14 +17,14 @@ typedef struct dns_hdr_flags_s {
   uint16_t rcode : 4;
 } dns_hdr_flags_t;
 
-typedef struct dns_head_s {
+typedef struct dns_hdr_s {
   uint16_t id;
   dns_hdr_flags_t flags;
   uint16_t qdcount;
   uint16_t ancount;
   uint16_t nscount;
   uint16_t arcount;
-} dns_head_t;
+} dns_hdr_t;
 
 typedef struct dns_qtn_s {
   char * name;
@@ -48,32 +48,40 @@ uint16_t read_short(char * buff) {
   return *(buff) << 8 | *(buff + 1);
 }
 
-void encode_dns_head(dns_head_t * head, char * buff, short len) {
+// encode dns header for transmission (struct -> buffer)
+void encode_dns_head(dns_hdr_t * head, char * buff, short len) {
   // assume that buff is already initialised with sufficient length
 
-  head->id = read_short(&buff[0]);
-
-  // get flags
-  head->flags.qr = (buff[2] >> 7) & 0x1;
-  head->flags.optcode = (buff[2] >> 4) & 0xF;
-  head->flags.aa = (buff[2] >> 2) & 0x1;
-  head->flags.tc = (buff[2] >> 1) & 0x1;
-  head->flags.rd = (buff[2]) & 0x1;
-  head->flags.ra = (buff[3] >> 7) & 0x1;
-  head->flags.z = (buff[3] >> 4) & 0x7;
-  head->flags.rcode = (buff[3]) & 0xF;
-  
-  head->qdcount = read_short(&buff[4]);
-  head->ancount = read_short(&buff[6]);
-  head->nscount = read_short(&buff[8]);
-  head->arcount = read_short(&buff[10]);
-  
 }
 
+// encode dns question for transmission (struct -> buffer)
 void encode_dns_qtn(dns_qtn_t * qtn, char * buff, short len);
+
+// encode dns message for transmission (struct -> buffer)
 void encode_dns_msg(dns_msg_t * msg, char * buff, short len);
 
-void decode_dns_head(char * buff, short len, dns_msg_t * qtn);
+// decode received dns header (buffer -> struct)
+void decode_dns_head(char * buff, short len, dns_hdr_t * hdr)
+{
+  hdr->id = read_short(&buff[0]);
+
+  // get flags
+  hdr->flags.qr = (buff[2] >> 7) & 0x1;
+  hdr->flags.optcode = (buff[2] >> 4) & 0xF;
+  hdr->flags.aa = (buff[2] >> 2) & 0x1;
+  hdr->flags.tc = (buff[2] >> 1) & 0x1;
+  hdr->flags.rd = (buff[2]) & 0x1;
+  hdr->flags.ra = (buff[3] >> 7) & 0x1;
+  hdr->flags.z = (buff[3] >> 4) & 0x7;
+  hdr->flags.rcode = (buff[3]) & 0xF;
+  
+  hdr->qdcount = read_short(&buff[4]);
+  hdr->ancount = read_short(&buff[6]);
+  hdr->nscount = read_short(&buff[8]);
+  hdr->arcount = read_short(&buff[10]);
+}
+
+// decode received dns message (buffer -> struct)
 void decode_dns_msg(char * buff, short len, dns_msg_t * msg);
 
 int main()
